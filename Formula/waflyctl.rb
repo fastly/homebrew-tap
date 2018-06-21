5ec6fa76ffb44bc6eb8ef3d9012daffdc18f8807
Jose Enrique Hernandez <josehelps@gmail.com>
2018-06-21 12:21:05 -0400

    Update waflyctl.rb

--- Formula/waflyctl.rb
+++ Formula/waflyctl.rb
@@ -1,7 +1,7 @@
 class Waflyctl < Formula
   desc "Allows you to provision a Fastly waf object with pre-determine rules, OWASP config, response, and logging endpoints. Also manage rules, and their status."
   homepage "https://github.com/fastly/waflyctl"
-  url "https://github.com/fastly/waflyctl/releases/download/0.1.4/waflyctl_0.1.4_darwin_amd64.tar.gz"
+  url "https://github.com/fastly/waflyctl/releases/download/0.1.4/waflyctl_0.1.4_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "0.1.4"
   sha256 "a2ad39e92a253a16376f0a676170e1812edb61cd2ee3076bac5b704062347de4"
 
