0fbd48372e358212b25865e8292aee18beb566a0
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-23 17:52:32 -0400

    Brew formula update for waflyctl version v0.1.6

--- Formula/waflyctl.rb
+++ Formula/waflyctl.rb
@@ -1,9 +1,9 @@
 class Waflyctl < Formula
   desc "Allows you to provision a Fastly waf object with pre-determine rules, OWASP config, response, and logging endpoints. Also manage rules, and their status."
   homepage "https://github.com/fastly/waflyctl"
-  url "https://github.com/fastly/waflyctl/releases/download/0.1.5/waflyctl_0.1.5_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.1.5"
-  sha256 "56376489d7a1a861c2c9558cfe24f1cd14db962eadfaaaf5d85672d16716ef91"
+  url "https://github.com/fastly/waflyctl/releases/download/v0.1.6/waflyctl_0.1.6_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.1.6"
+  sha256 "777b653d7d7bc5b1e29f9e0062862d8f6c4e66c13be025e0d9416c91513d67e9"
 
   def install
     bin.install "waflyctl"
