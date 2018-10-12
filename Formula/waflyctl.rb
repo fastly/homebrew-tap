4b7ac3e5a6eb9b117baf959e8da46b3135ac7f0a
Federico G. Schwindt <fgsch@lodoss.net>
2018-10-12 03:18:02 +0100

    Add --HEAD support

--- Formula/waflyctl.rb
+++ Formula/waflyctl.rb
@@ -5,7 +5,22 @@ class Waflyctl < Formula
   version "0.1.8"
   sha256 "2d4a3f7aaf6dddf649ba14ac9d4b54ce2817fb15752bfd43ac394e929bf5ad51"
 
+  head do
+    url "https://github.com/fastly/waflyctl.git"
+    depends_on "dep" => :build
+    depends_on "go" => :build
+  end
+
   def install
+    if build.head?
+      ENV["GOPATH"] = buildpath
+      (buildpath/"src/github.com/fastly/waflyctl").install buildpath.children
+      cd "src/github.com/fastly/waflyctl" do
+        system "dep", "ensure", "-vendor-only"
+        system "go", "build", "-o", buildpath/"waflyctl"
+      end
+    end
+
     bin.install "waflyctl"
   end
 
