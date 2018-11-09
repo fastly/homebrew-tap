3a4393474fa9223eedbd1f310f8ce38dcec8b024
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-09 15:31:00 +0000

    Brew formula update for waflyctl version v0.2.0

--- Formula/waflyctl.rb
+++ Formula/waflyctl.rb
@@ -1,26 +1,11 @@
 class Waflyctl < Formula
   desc "Allows you to provision a Fastly waf object with pre-determine rules, OWASP config, response, and logging endpoints. Also manage rules, and their status."
   homepage "https://github.com/fastly/waflyctl"
-  url "https://github.com/fastly/waflyctl/releases/download/v0.1.8/waflyctl_0.1.8_darwin_amd64.tar.gz"
-  version "0.1.8"
-  sha256 "2d4a3f7aaf6dddf649ba14ac9d4b54ce2817fb15752bfd43ac394e929bf5ad51"
-
-  head do
-    url "https://github.com/fastly/waflyctl.git"
-    depends_on "dep" => :build
-    depends_on "go" => :build
-  end
+  url "https://github.com/fastly/waflyctl/releases/download/v0.2.0/waflyctl_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "9f824dfe5f895ac5b557a2366179a3233492ffc81d3d77a44d9ef3eab238793d"
 
   def install
-    if build.head?
-      ENV["GOPATH"] = buildpath
-      (buildpath/"src/github.com/fastly/waflyctl").install buildpath.children
-      cd "src/github.com/fastly/waflyctl" do
-        system "dep", "ensure", "-vendor-only"
-        system "go", "build", "-o", buildpath/"waflyctl"
-      end
-    end
-
     bin.install "waflyctl"
   end
 
