a740a3831bec60e92f0487e6dfcc8e321afc075b
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-16 13:21:36 +0000

    Brew formula update for fastly version v0.25.2

--- Formula/fastly.rb
+++ Formula/fastly.rb
@@ -5,20 +5,20 @@
 class Fastly < Formula
   desc "Fastly CLI"
   homepage "https://github.com/fastly/cli"
-  version "0.25.1"
+  version "0.25.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/fastly/cli/releases/download/v0.25.1/fastly_v0.25.1_darwin-amd64.tar.gz"
-    sha256 "f7e3bfa01558594241062f11b568f0d788b15871e61637fecde17998f32e6377"
+    url "https://github.com/fastly/cli/releases/download/v0.25.2/fastly_v0.25.2_darwin-amd64.tar.gz"
+    sha256 "468d53fef3534a4f63963ff1ca455286d92445d231fe4126d1321546665b92f4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fastly/cli/releases/download/v0.25.1/fastly_v0.25.1_linux-amd64.tar.gz"
-    sha256 "3b136812910a6d68c0453d2e09f9738e021932d20928c87befaa40e2d0beaee9"
+    url "https://github.com/fastly/cli/releases/download/v0.25.2/fastly_v0.25.2_linux-amd64.tar.gz"
+    sha256 "53ab12d96803865108f0d42a8991b4b83b81764ba26d5b6635d5424376a00091"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/fastly/cli/releases/download/v0.25.1/fastly_v0.25.1_linux-arm64.tar.gz"
-    sha256 "08bc606c78bf45ebf2583d3137f5e7daae550c50cb4d8e0c739900f8e4d756f4"
+    url "https://github.com/fastly/cli/releases/download/v0.25.2/fastly_v0.25.2_linux-arm64.tar.gz"
+    sha256 "21c327eb9f04f414285185a67ead7bd1333de9d57c12ee21e005b4de865df384"
   end
 
   head do
