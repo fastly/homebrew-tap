85dbdc4bd6fb90c512e349599f562a66a4ce586e
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-19 15:21:39 +0100

    Brew formula update for fastly version v0.41.0

--- Formula/fastly.rb
+++ Formula/fastly.rb
@@ -5,28 +5,28 @@
 class Fastly < Formula
   desc "Fastly CLI"
   homepage "https://github.com/fastly/cli"
-  version "0.40.2"
+  version "0.41.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/fastly/cli/releases/download/v0.40.2/fastly_v0.40.2_darwin-amd64.tar.gz"
-      sha256 "4307354062317ad7dff2af8c4eaab3695151b49bd09c84940ba941818638087e"
+      url "https://github.com/fastly/cli/releases/download/v0.41.0/fastly_v0.41.0_darwin-amd64.tar.gz"
+      sha256 "4d4a9840a3dbb9ff11a6281176c943f59d631a8ba3e1e3565f27423442fabcf0"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/fastly/cli/releases/download/v0.40.2/fastly_v0.40.2_darwin-arm64.tar.gz"
-      sha256 "49c24e971d8047d2a302549f83e34e76a0c15c01e931fbc0444fdab58dcf010b"
+      url "https://github.com/fastly/cli/releases/download/v0.41.0/fastly_v0.41.0_darwin-arm64.tar.gz"
+      sha256 "c91a3ae8cf55cf95805149bc212cafaac3bf8d3d06eaeda933cf4c3c3ad07b0d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/fastly/cli/releases/download/v0.40.2/fastly_v0.40.2_linux-amd64.tar.gz"
-      sha256 "960950dc277cae4b46382e0003e2d4f761a670327a0a7bd213bb2a19e1c31855"
+      url "https://github.com/fastly/cli/releases/download/v0.41.0/fastly_v0.41.0_linux-amd64.tar.gz"
+      sha256 "ae882b4598869d3f4c496374e5e553bdc0f59c53e1bf254e86f1a2190008619a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/fastly/cli/releases/download/v0.40.2/fastly_v0.40.2_linux-arm64.tar.gz"
-      sha256 "c0781aecf423ec48b923d33a6af92bf21819439920fcbf96d84dfef7ef820f3a"
+      url "https://github.com/fastly/cli/releases/download/v0.41.0/fastly_v0.41.0_linux-arm64.tar.gz"
+      sha256 "f2bc71784b2d9c0b7029dbcb46e1da2e7103a1573e5420af0a1f8875737f7c5b"
     end
   end
 
